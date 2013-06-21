bf11605bf9981d6927e93992563a0937abeed6a9
Trejkaz <trejkaz@trypticon.org>
2013-06-21 22:34:54 +1000

    Examples always run off the local copy instead of the installed gem (easier to work with)

--- example/Includes.rb
+++ example/Includes.rb
@@ -1,7 +1,8 @@
-require 'rubygems'
+$LOAD_PATH << File.join(File.dirname(__FILE__), "../lib")
 require 'rubysynth'
 
 require 'ExampleSounds'
+require 'PhaseModulationExamples'
 require 'DemoSong'
 require 'SweetChildOMine'
 require 'SweetDreams'
